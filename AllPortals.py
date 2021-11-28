957a08874ce05f8ec1f31af82b566acc302d7e51
TheTalkingMime <80478795+TheTalkingMime@users.noreply.github.com>
2021-11-27 23:53:39 -0500

    Add files via upload

---
+++ AllPortals.py
@@ -0,0 +1,150 @@
+import os
+import numpy as np
+from utils import *
+import matplotlib.pyplot as plt
+
+
+first_strongholds = []
+sh_per_ring = [3, 6, 10, 15, 21, 28, 36, 10]
+magnitude_per_ring = [2048, 5120, 8192, 11264, 14336, 17408, 20480, 23552]
+count = 0
+updateCount(count)
+
+img = plt.imread("rings.png")
+plt.imshow(img, aspect="equal", extent=[-24320, 24320, -24320, 24320])
+plt.axis("off")
+plt.savefig("output.png", bbox_inches="tight", transparent=True)
+
+prev = (0, 0)
+for i in range(1, 9):
+    sh = getIntInput("Stronghold in ring " + str(i) + ":")
+    first_strongholds.append((sh[0], sh[1]))
+
+    count += 1
+    updateCount(count)
+    plt.scatter(sh[0], sh[1], c="green", s=40)
+    plt.arrow(
+        prev[0],
+        prev[1],
+        sh[0] - prev[0],
+        sh[1] - prev[1],
+        color="green",
+        width=0.0001,
+        head_width=0,
+        head_length=0,
+        length_includes_head=True,
+    )
+    plt.savefig("output.png", bbox_inches="tight", transparent=True)
+    prev = sh
+
+new_strongholds = []
+
+# Predict location of all the other strongholds
+for i in range(len(first_strongholds)):
+    x, z = first_strongholds[i]
+    magnitude = magnitude_per_ring[i]
+    vec1 = np.array([x, z])
+    vec2 = np.array([1, 0])
+    ang = np.arctan2(vec1[1], vec1[0]) - np.arctan2(vec2[1], vec2[0])
+    print("Ring", i + 1, ang)
+    for j in range(sh_per_ring[i] - 1):
+        ang += (2 * np.pi) / sh_per_ring[i]
+        new_x = magnitude * np.cos(ang)
+        new_z = magnitude * np.sin(ang)
+        new_strongholds.append((round(new_x), round(new_z)))
+        print(round(new_x), round(new_z), ang)
+
+paths, nearest_idx = generatePath(new_strongholds, first_strongholds[-1])
+
+nearest = new_strongholds[nearest_idx]
+eighth_ring = new_strongholds[-9:]
+curr = nearest_idx
+
+
+plt.scatter(*zip(*new_strongholds), c="gray", s=20)
+plt.savefig("output.png", bbox_inches="tight", transparent=True)
+
+print("\nPress enter to receive next stronghold or 'h' for alternative commands\n")
+
+completed = first_strongholds
+unfinished = new_strongholds
+prev = completed[-1]
+c2 = False
+noGraph = False
+while count < 128:
+    # dist = get_dist(new_strongholds[next], new_strongholds[int(prev)])
+    completed.append(prev)
+
+    sh = unfinished[curr]
+    sh_n = (round(sh[0] / 8), round(sh[1] / 8))
+
+    line, point = graphAddSH(prev, sh, "blue", c2)
+    if not noGraph:
+        graphAddSH(
+            completed[-2], prev, "green", c2
+        )  # Do not mark sh as finished if there was a reset
+    plt.savefig("output.png", bbox_inches="tight", transparent=True)
+
+    noGraph = False
+    c2 = False
+    prev = sh
+
+    while True:
+        prompt = "Stronghold " + str(count + 1) + ":\t" + str(sh_n) + "\n"
+
+        if sh in eighth_ring:
+            prompt += "8th ring, there could be no stronghold\n"
+
+        user = input(prompt)
+
+        if user == "":
+            count += 1
+            break
+        elif user == "h":
+            printHelp()
+        elif user == "0":
+            c2 = True
+            point.remove()
+            plt.draw()
+            break
+        elif user == "e":
+            new_count = getIntInput(
+                "Type in the new number of strongholds you have completed\n"
+            )[0]
+            count = new_count
+            updateCount(count)
+        elif user == "d" or user == "d*":
+            pos = (0, 0)
+            if user == "d*":
+                pos = tuple(
+                    getIntInput(
+                        "Type out your x and z coordinates you want to start pathfinding from (OW):\n"
+                    )
+                )
+            unfinished = list(set(unfinished) - set(completed))
+            paths, curr = generatePath(unfinished, pos)
+
+            # noGraph = True
+
+            sh = unfinished[curr]  # Next stronghold starting from 0,0
+            prev = sh
+            completed.append(pos)
+            # Update prompt to nether coords
+            sh_n = (round(sh[0] / 8), round(sh[1] / 8))
+
+            # Clean graph's in progress stuff
+            try:
+                line.remove()
+                point.remove()
+            except Exception as e:
+                print(
+                    "Must've forgotten bed after finding empty 8th ring sh, or else something went really wrong"
+                )
+            plt.draw()
+
+            graphAddSH(pos, sh, "blue", c2)
+
+            plt.savefig("output.png", bbox_inches="tight", transparent=True)
+
+    curr = paths[curr]
+    updateCount(count)
---
+++ utils.py
@@ -0,0 +1,115 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def is_prime(n):
+    if n % 2 == 0 and n > 2:
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+
+
+def generatePath(shs, pos):
+    f = open("strongholds.qs", "w+")
+    print(str(len(shs)), "0", file=f)
+    for sh in shs:
+        print(str(sh[0]), str(sh[1]), file=f)
+    f.close()
+    input(
+        "Use the strongholds.qs file in this directory to run concorde pathfinding, then once you solve and save, press enter"
+    )
+
+    f = open("strongholds.qs", "r")
+    lines = f.readlines()
+    f.close()
+
+    paths = {}
+    for i in range(len(shs) + 1, len(lines)):
+        start, end, dist = lines[i].split()
+        paths[int(start)] = int(end)
+    rev_paths = dict((v, k) for k, v in paths.items())
+
+    new_x, new_z = zip(*shs)
+
+    # Have to scale numbers down due to overflow errors
+    new_x = np.array(list(new_x)) / 100
+    new_z = np.array(list(new_z)) / 100
+    pos = (pos[0] / 100, pos[1] / 100)
+
+    dists = np.sqrt(((new_x - pos[0]) ** 2) + ((new_z - pos[1]) ** 2))
+    nearest_idx = np.argmin(dists)
+    normal = get_dist(shs[nearest_idx], shs[paths[nearest_idx]])
+    reverse = get_dist(shs[nearest_idx], shs[rev_paths[nearest_idx]])
+    if normal > reverse:
+        paths = rev_paths
+    return paths, nearest_idx
+
+
+def get_dist(x1, x2):
+    return np.sqrt(((x1[0] - x2[0]) ** 2) + ((x1[1] - x2[1]) ** 2))
+
+
+def updateCount(count):
+    f = open("sh_count.txt", "w+")
+    facts = open("fun_facts.txt", "w+")
+
+    lines = [str(count) + "/128\n"]
+
+    if count == 69:
+        facts.writelines(["nice.\n"])
+    if count == 71:
+        facts.writelines(["BRA7-1L\n"])
+    if is_prime(count):
+        facts.writelines(["(prime)\n"])
+
+    f.writelines(lines)
+
+    f.close()
+    facts.close()
+
+
+def printHelp():
+    print(
+        "\n-------------------------------------------------------------------------------------------\n"
+        "All valid commands\n"
+        + "Enter\t\t-\t"
+        + "Marks stronghold as complete and updates count\n"
+        + "0\t\t-\t"
+        + "Marks that you checked the coordinates but there was no stronghold\n"
+        + "e\t\t-\t"
+        + "Allows you to manually adjust the stronghold count\n"
+        + "d\t\t-\t"
+        + "Mark that you went back to spawn, and restart pathfinding from 0 0\n"
+        + "d*\t\t-\t"
+        + "Restart pathfinding from specific coordinates\n"
+        + "-------------------------------------------------------------------------------------------\n"
+    )
+
+
+def getIntInput(prompt):
+    while True:
+        try:
+            integers = input(prompt).split()
+            results = list(map(int, integers))
+            return results
+        except Exception as e:
+            print("Integer(s) only input. Seperate multiple numbers with spaces")
+            continue
+
+
+def graphAddSH(prev, sh, col1, flag):
+    line = plt.arrow(
+        prev[0],
+        prev[1],
+        sh[0] - prev[0],
+        sh[1] - prev[1],
+        color=col1,
+        width=0.0001,
+        head_width=0,
+        head_length=0,
+        length_includes_head=True,
+    )
+    point = None
+    if not flag:
+        point = plt.scatter(sh[0], sh[1], c=col1, s=30)
+    return line, point
